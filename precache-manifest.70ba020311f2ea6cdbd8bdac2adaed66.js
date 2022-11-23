853909f81fb36a2ff2d92cdafb6b80945a36948a
ryx111 <catpaak@gmail.com>
2022-11-22 18:53:49 -0800

    Updates

---
+++ precache-manifest.70ba020311f2ea6cdbd8bdac2adaed66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00560423f716fe1a43a92ab101c53190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6023d78ea85c5d9f88d8",
+    "url": "/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "a13d13f8cf3aa68c8210",
+    "url": "/static/css/main.0d2db1e1.chunk.css"
+  },
+  {
+    "revision": "6023d78ea85c5d9f88d8",
+    "url": "/static/js/2.e2141eab.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.e2141eab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a13d13f8cf3aa68c8210",
+    "url": "/static/js/main.a4600ddc.chunk.js"
+  },
+  {
+    "revision": "9473ea17c386807a80e6",
+    "url": "/static/js/runtime-main.1262bfa0.js"
+  }
+]);
